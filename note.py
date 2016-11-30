8b97cdda639b14c72bc0a621433f220cf871d110
Simon Klit <boutrupsimon@gmail.com>
2016-11-29 23:31:48 -0800

    0.2 - introducing todo notes

--- note.py
+++ note.py
@@ -43,7 +43,7 @@ requests = impmodule("requests")
 
 appname = "PyTNote"
 appauthor = "SimonKlitJohnson"
-tag_name = 0.13
+tag_name = 0.2
 args = sys.argv[1:]
 notes = []
 
@@ -65,20 +65,32 @@ def write_and_read(msg=None):
     read_notes()
 
 
-def write_notes():
-    with open(file, 'w+') as notefile:
-        notefile.write(json.dumps(notes))
+def write_notes(nts=None):
+    if nts != None:
+        with open(file, 'w+') as notefile:
+            notefile.write(json.dumps(nts))
+    else:
+        with open(file, 'w+') as notefile:
+            notefile.write(json.dumps(notes))
 
 
 def read_notes():
     if os.path.exists(file):
         with open(file, 'r') as outfile:
             outfile = json.loads(outfile.read())
+            sorted(outfile, key=lambda k: k['creation_time']) # sort by time created
             if len(outfile) > 0:
                 prnt("Your notes:", True)
                 i = 1
                 for note in outfile: # Convert timestamp to datetime and output the contents of the note.
-                    prnt("("+str(i)+") "+"\033[4m" + datetime.datetime.fromtimestamp(int(note['creation_time'])).strftime('%d/%m - %H:%M') + "\033[0m: \"" +note['content'] + "\"")
+                    if note['type'] == "note":
+                        prnt("("+str(i)+") "+"\033[4m" + datetime.datetime.fromtimestamp(int(note['creation_time'])).strftime('%d/%m - %H:%M') + "\033[0m:     \"" +note['content'] + "\"")
+                    else:
+                        if note['status'] == True:
+                            prnt("("+str(i)+") "+"\033[4m" + datetime.datetime.fromtimestamp(int(note['creation_time'])).strftime('%d/%m - %H:%M') + "\033[0m: [X] \"" +note['content'] + "\"")
+
+                        else:
+                            prnt("("+str(i)+") "+"\033[4m" + datetime.datetime.fromtimestamp(int(note['creation_time'])).strftime('%d/%m - %H:%M') + "\033[0m: [ ] \"" +note['content'] + "\"")
                     i = i + 1
             else:
                 prnt("You have no notes at this time.", True)
@@ -102,6 +114,59 @@ def check_update(retrn=True):
         return False
         pass
 
+def install_todo(args=None):
+    try:
+        with open(appdirs.user_data_dir(appname, appauthor) + '/todo', 'w+') as todo:
+            todo.write("#!/usr/bin/env python3\nimport os, sys\nos.system('note -t ' + ' '.join(sys.argv[1:]))")
+        
+        os.system("chmod +x \""+appdirs.user_data_dir(appname, appauthor) + "/todo\"")
+        os.system("mv \"" + appdirs.user_data_dir(appname, appauthor) + "/todo\" \"/usr/local/bin/todo\"" )
+        
+        prnt("\nTodo extension succesfully installed. Try running \"todo\" in your terminal.\n")
+        if args != None:
+            os.system("todo -t " + " ".join(args))
+    except:
+        prnt("Something went wrong during the installation of the todo extension. Make sure you can write to /usr/local/bin.", True)
+
+def deletenote(args):
+    try:
+        with open(file, 'r') as notefile:
+            notes = json.loads(notefile.read())
+    except IOError:
+        prnt("You have no notes at this time.\n", True)
+        sys.exit(0)
+    try:
+        del(notes[int(args[1]) - 1])
+        write_notes(notes)
+        prnt("Note %s deleted.\n" % (str(int(args[1]))), True)
+    except IndexError:
+        prnt("Note does not exist. Not deleted.\n", True)
+        pass
+
+    read_notes()
+
+def switchnote(args):
+    try:
+        with open(file, 'r') as notefile:
+            notes = json.loads(notefile.read())
+    except IOError:
+        prnt("You have no notes at this time.\n", True)
+        sys.exit(0)
+    try:
+        if notes[int(args[1]) -1]['type'] == "note":
+            notes[int(args[1]) -1]['type'] = "todo"
+            notes[int(args[1]) -1]['status'] = False
+        else:
+            notes[int(args[1]) -1]['type'] = "note"
+        write_notes(notes)
+        prnt("Note succesfully switched.\n",True)
+        read_notes()
+        print(" ")
+        sys.exit(0)
+    except IndexError:
+        prnt("Note does not exist. Not switched.\n", True)
+        read_notes()
+
 file = appdirs.user_data_dir(appname, appauthor) + "/pytnote.json" # Get OS-specific AppDirectory, in which to store the notes.
 
 if not os.path.isdir(appdirs.user_data_dir(appname, appauthor)): # Create AppDirectory folder if it doesn't exists.
@@ -114,21 +179,7 @@ elif args[0] == "-d":
     '''
         Deletes note by removing from list and then rewriting to notefile.
     '''
-    try:
-        with open(file, 'r') as notefile:
-            notes = json.loads(notefile.read())
-    except IOError:
-        prnt("You have no notes at this time.\n", True)
-        sys.exit(0)
-    try:
-        del(notes[int(args[1]) - 1])
-        write_notes()
-        prnt("Note %s deleted.\n" % (str(int(args[1]))), True)
-    except IndexError:
-        prnt("Note does not exist. Not deleted.\n", True)
-        pass
-
-    read_notes()
+    deletenote(args)
 
 elif args[0] == "-c":
     '''
@@ -165,17 +216,21 @@ elif args[0] == "-cl":
 
 elif args[0] == "-h":
 
-    prnt("Help:\n",True)
-    prnt("\033[1mno arguments\033[0m\t- lists all your notes and their times of creation")
-    prnt("\033[1m*\033[0m\t\t- creates a new note consisting of all arguments joined.\n")
-    prnt("\033[1m-c [note no.]\033[0m\t- copies the contents of the note to the clipboard")
-    prnt("\033[1m-cl\033[0m\t\t- clears all notes (asks you to confirm before doing so)")
-    prnt("\033[1m-d [note no.]\033[0m\t- deletes note with corresponding no.")
-    prnt("\033[1m-e [note no.]\033[0m\t- edits note with corresponding no.")
-    prnt("\033[1m-h\033[0m\t\t- lists this help menu")
-    prnt("\033[1m-u\033[0m\t\t- update PyTNote if update is available")
-    prnt("\033[1m-uninstall\033[0m\t\t- uninstall PyTNote (asks you to confirm before doing so)")
-    prnt("\033[1m-s [note no.]\033[0m\t- shares note with corresponding no. to hastebin (and copies URL to clipboard)")
+    prnt("PyTNote help:\n",True)
+    prnt("\033[1mno arguments\033[0m\t - lists all your notes and their times of creation")
+    prnt("\033[1m*\033[0m\t\t - creates a new note consisting of all arguments joined.\n")
+    prnt("\033[1m-c [note no.]\033[0m\t - copies the contents of the note to the clipboard")
+    prnt("\033[1m-cl\033[0m\t\t - clears all notes (asks you to confirm before doing so)")
+    prnt("\033[1m-d [note no.]\033[0m\t - deletes note with corresponding no.")
+    prnt("\033[1m-e [note no.]\033[0m\t - edits note with corresponding no.")
+    prnt("\033[1m-h\033[0m\t\t - lists this help menu")
+    prnt("\033[1m-t\033[0m\t\t - create todo (offers to install todo extension on non-Windows)")
+    prnt("\033[1m-t -h\033[0m\t\t - list the help menu for todos")
+    prnt("\033[1m-u\033[0m\t\t - update PyTNote if update is available")
+    prnt("\033[1m-uninstall\033[0m\t - uninstall PyTNote (asks you to confirm before doing so)")
+    print("\033[1m-uninstall -todo\033[0m - uninstall only the todo extension")
+    prnt("\033[1m-s [note no.]\033[0m\t - shares note with corresponding no. to hastebin (and copies URL to clipboard)")
+    prnt("\033[1m-sw [note no.]\033[0m\t - switches note with corresponding no. between begin a todo and a note")
 
 
 elif args[0] == "-s":
@@ -190,7 +245,14 @@ elif args[0] == "-s":
         prnt("You have no notes at this time.\n", True)
         sys.exit(0)
     try:
-        r = requests.post("http://hastebin.com/documents", data=notes[int(args[1]) -1]['content'])
+        if notes[int(args[1]) -1]['type'] == "note":
+            r = requests.post("http://hastebin.com/documents", data=notes[int(args[1]) -1]['content'])
+        else:
+            if(notes[int(args[1]) -1]['status'] == True):
+                data = "[X] " + notes[int(args[1]) -1]['content']
+            else:
+                data = "[ ] " + notes[int(args[1]) -1]['content']
+            r = requests.post("http://hastebin.com/documents", data=data)
         r = json.loads(r.text)
         url = "http://hastebin.com/" + r['key']
         pyperclip.copy(url)
@@ -203,6 +265,8 @@ elif args[0] == "-s":
         prnt("Unable to share note. Do you have an internet connection?\n", True)
         read_notes()
 
+elif args[0] == "-sw":
+    switchnote(args)
 
 elif args[0] == "-e":
     '''
@@ -283,6 +347,65 @@ elif args[0] == "-e":
         prnt("Note does not exist. Not editable.\n", True)
         read_notes()
 
+elif args[0] == "-t":
+    if not sys.platform == "win32":
+        if not os.path.exists("/usr/local/bin/todo"):
+            prnt("Do you want to install the todo extension? This allows you to create todo's by simply typing \"todo\". (y/n)", True)
+            prompt = input("> ")
+            if prompt == "y":
+                install_todo()
+
+    if len(args) > 1:
+        args = args[1:]
+        if os.path.exists(file): # If we already have some notes saved
+            with open(file, 'r') as notefile:
+                notes = json.loads(notefile.read())  
+
+        if args[0] == "-m": # Mark todo as done
+            try:
+                if notes[int(args[1]) -1]['status'] == False:
+                    notes[int(args[1]) -1]['status'] = True
+                    write_and_read()
+                    print(" ")
+                    sys.exit(0)
+                else:
+                    notes[int(args[1]) -1]['status'] = False
+                    write_and_read()
+                    print(" ")  
+                    sys.exit(0)
+            except IndexError:
+                prnt("\nNote does not exist. Not markable.\n", True)
+                read_notes()
+                sys.exit(0)
+            except KeyError:
+                prnt("Note is not a todo. Not markable.\n", True)
+                read_notes()
+                sys.exit(0)
+
+        if args[0] == "-d":
+            deletenote(args)
+            print(" ")
+            sys.exit(0)
+
+        elif args[0] == "-h":
+            prnt("PyTNote todo extension help:\n",True)
+            prnt("\033[1mno arguments\033[0m\t- lists all your notes and their times of creation")
+            prnt("\033[1m*\033[0m\t\t- creates a new todo consisting of all arguments joined.\n")
+            prnt("\033[1m-d [note no.]\033[0m\t- deletes note with corresponding no.")
+            prnt("\033[1m-h\033[0m\t\t- shows this help menu")
+            prnt("\033[1m-m [note no.]\033[0m\t- marks the note as the opposite of what it currently is\n")
+            prnt("\033[1m-sw [note no.]\033[0m\t- switches note with corresponding no. between begin a todo and a note\n")
+            sys.exit(0)
+
+        elif args[0] == "-sw":
+            switchnote(args)
+        else:
+            notes.append({"creation_time":int(time.time()),"content":" ".join(args), "type": "todo", "status": False})
+        write_and_read()
+        sys.exit(0)
+    else:
+        read_notes()
+
 elif args[0] == "-u":
     '''
         Update the script by downloading latest release zipball, unzipping and asking user to replace note script with new one.
@@ -330,13 +453,31 @@ elif args[0] == "-u":
     else:
         prnt("No update available.\n", True)
         read_notes()
+
 elif args[0] == "-uninstall":
+
+    if args[1] == "-todo": # Specifically uninstall the todo extension
+        prompt = input("Are you sure you want to uninstall the todo extension? (y/n)\n> ")
+        if prompt == "y":
+            if os.path.exists("/usr/local/bin/todo"):
+                os.remove("/usr/local/bin/todo")
+                prnt("Todo extension removed.", True)
+                sys.exit(0)
+            else:
+                prnt("\nTodo extension not installed.\n",True)
+                sys.exit(0)
+        else:
+            prnt("Aborting uninstallation.",True)
+            sys.exit(0)
+
     prompt = input("Are you sure you want to uninstall PyTNote? (y/n)\n> ")
     if prompt == "y":
         prompt = input("Do you want to delete all of your notes? (y/n)\n> ")
         if prompt == "y":
             shutil.rmtree(appdirs.user_data_dir(appname, appauthor))
         os.remove(os.path.dirname(os.path.realpath(__file__)) + "/" + os.path.basename(__file__))
+        if os.path.exists("/usr/local/bin/todo"):
+            os.remove("/usr/local/bin/todo")
         prnt("\nPyTNote uninstalled. Sad to see you go!\n", True)
         sys.exit(0)
     else:
@@ -345,7 +486,7 @@ else: # If the text passed does not match any of our commands = create new note.
     if os.path.exists(file): # If we already have some notes saved
         with open(file, 'r') as notefile:
             notes = json.loads(notefile.read())   
-    notes.append({"creation_time":int(time.time()),"content":" ".join(args)})
+    notes.append({"creation_time":int(time.time()),"content":" ".join(args), "type": "note"})
     write_and_read()        
 
 if "-u" not in args: # If we're not trying to update the script, check if an update is available.
--- readme.md
+++ readme.md
@@ -2,7 +2,7 @@
 ## PyTNote (Python Terminal Notes) is a simple note manager for your terminal.
 
 PyTNote (Python Terminal Notes) is a simple note manager for your terminal.
-It allows you to quickly and easily create and manage notes without ever leaving your terminal workflow.
+It allows you to quickly and easily create and manage notes (or todo notes) without ever leaving your terminal workflow.
 
 Creating a new note is as simple as typing "note Remember to update readme".
 
@@ -10,32 +10,57 @@ Creating a new note is as simple as typing "note Remember to update readme".
 
 Stay on top of your productivity with PyTNote!
 
+As of release 0.2 the script also includes a so-called "todo extension". This extension creates a small, separate script which allows you to create todo notes that can be marked as either done or not done. You do not have to install the todo extension as a separate script to gain access to the todo functionality, though. Instead of typing "todo", simply type "note -t".
+
+Example: "note -t buy potatoes" is the same as "todo buy potatoes", as "note -t -m 1" is the same as "todo -m 1".
+
 This software has no license, so do whatever you want to with it. I'd prefer it if you didn't sell it though.
 
 ## Getting started
+
+Using PyTNote is really simple, but the script has a lot of extra functionality that can be nifty and nice to know about.
+
 #### Arguments
 You can use the following arguments when running the script:
 ```
-no arguments       - lists all your notes and their times of creation
-*                  - creates a new note consisting of all arguments joined
+no arguments	   - lists all your notes and their times of creation
+*                  - creates a new note consisting of all arguments joined.
 
--c [note no.]      - copies the contents of the note to the clipboard
+-c [note no.]	   - copies the contents of the note to the clipboard
 -cl                - clears all notes (asks you to confirm before doing so)
--d [note no.]      - deletes note with corresponding id
--e [note no.]      - edits note with corresponding id
+-d [note no.]      - deletes note with corresponding no.
+-e [note no.]      - edits note with corresponding no.
 -h                 - lists this help menu
+-t                 - create todo (offers to install todo extension on non-Windows)
+-t -h              - list the help menu for todos
 -u                 - update PyTNote if update is available
 -uninstall         - uninstall PyTNote (asks you to confirm before doing so)
+-uninstall -todo   - uninstall only the todo extension
 -s [note no.]      - shares note with corresponding no. to hastebin (and copies URL to clipboard)
+-sw [note no.]     - switches note with corresponding no. between being a todo and a note
+```
+
+And you can use the following arguments when running the todo extension:
+```
+no arguments	   - lists all your notes and their times of creation
+*		           - creates a new todo consisting of all arguments joined.
+
+-d [note no.]	   - deletes note with corresponding no.
+-h		           - shows this help menu
+-m [note no.]	   - marks the note as the opposite of what it currently is (done/not done)
+
+-sw [note no.]	   - switches note with corresponding no. between begin a todo and a note
 ```
 
 #### Dependencies
+PyTNote requires Python 3.0 or newer to function.
+
 PyTNote requires the following modules to work:
 * appdirs (finds the correct place to store the notes depending on your OS)
 * pyperclip (allows you to copy the note to clipboard)
 * requests (allows you to share the note to hastebin.com)
 
-If you have pip installed, all of the modules will automatically be installed upon running the script.
+But don't fret; if you have pip installed, all of the modules will automatically be installed upon running the script.
 
 ### macOS and Linux
 Installing PyTNote on macOS and Linux distributions is as simple as running the following one-liner in your terminal:
